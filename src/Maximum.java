2ad7301e7c927bf237ffbbb11b853c10631cd403
YayCover9000 <yassuanf@googlemail.com>
2022-12-23 20:22:08 +0100

    Char works now

--- src/Maximum.java
+++ src/Maximum.java
@@ -4,9 +4,11 @@ public class Maximum {
     public static void main(String [] args){
 
         double ausgabe;
-        ausgabe = Maximum.max();
+        char ausgabe2;
+        ausgabe = Maximum.maxDouble();
+        ausgabe2 = Maximum.maxChar();
     }
-    public static double max() {
+    public static double maxDouble() {
         System.out.println("Hello world!");
         System.out.println("gleit1 bitte");
         double gleit1 = sc.nextDouble();
@@ -29,9 +31,12 @@ public class Maximum {
         }
         return 0;
     }
-    public static char max(char c1, char c2) {
+    public static char maxChar() {
+        System.out.println("PLs give me Char1");
          char c1 = sc.next().charAt(0);
-        if(char c1 > char c2){
+        System.out.println("PLs give me Char2");
+        char c2 = sc.next().charAt(0);
+        if(c1 > c2){
             System.out.println(c1 + " c1 ist größer");
         }else{
             System.out.println("c2 ist größer");
