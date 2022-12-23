d0f916085054cdbd7e025b5ed4f0edcc7cb1e5ea
YayCover9000 <yassuanf@googlemail.com>
2022-12-23 20:01:51 +0100

    added char

--- src/Maximum.java
+++ src/Maximum.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 public class Maximum {
-
+    static Scanner sc = new Scanner(System.in);
     public static void main(String [] args){
+
         double ausgabe;
         ausgabe = Maximum.max();
     }
     public static double max() {
         System.out.println("Hello world!");
-        Scanner sc = new Scanner(System.in);
         System.out.println("gleit1 bitte");
         double gleit1 = sc.nextDouble();
         System.out.println("gleit2 bitte");
@@ -28,5 +28,14 @@ public class Maximum {
                 break;
         }
         return 0;
+    }
+    public static char max(char c1, char c2) {
+         char c1 = sc.next().charAt(0);
+        if(char c1 > char c2){
+            System.out.println(c1 + " c1 ist größer");
+        }else{
+            System.out.println("c2 ist größer");
+        }
+        return 'a';
     }
 }
